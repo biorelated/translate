2ff6d479f7e622a34212d77a765a33f4fb8833b1
biorelated <biorelated@gmail.com>
2015-08-25 16:15:36 +0300

    indentation

--- source/app.d
+++ source/app.d
@@ -7,33 +7,34 @@ import translate;
 
 //prints the amino acid sequence to the stderr
 void print_aa(FastaRecord seq){
-  writeln(">", seq.header,"\n",translate_seq(seq.sequence));
+    writeln(">", seq.header,"\n",translate_seq(seq.sequence));
 }
+
 
 //read each entry in the file and translate in parallel
 void translate_file(string infile){
-  auto seqs = fastaRecords(infile);
-  foreach(FastaRecord rec; seqs.parallel){
-    print_aa(rec);
-  }
+    auto seqs = fastaRecords(infile);
+    foreach(FastaRecord rec; seqs.parallel){
+        print_aa(rec);
+    }
 }
 
 //The main program
 void main(string[] args){
-  string inputfile;
-  string outputfile;
+    string inputfile;
+    string outputfile;
 
-  auto helpfile = getopt(args,
-      "inputfile|i","INPUT FILE", &inputfile,
-      "output|o", "OUTPUT FILE", &outputfile
-      );
+    auto helpfile = getopt(args,
+            "inputfile|i","INPUT FILE", &inputfile,
+            "output|o", "OUTPUT FILE", &outputfile
+            );
 
-  /* if (helpInfo.helpWanted){ */
-  /* defaultGetoptPrinter("A program to translate DNA to Protein",helpInfo.options); }*/
+    /* if (helpInfo.helpWanted){ */
+    /* defaultGetoptPrinter("A program to translate DNA to Protein",helpInfo.options); }*/
 
-  if(exists(inputfile)) {
-    translate_file(inputfile);
-  }else{
-    writeln("Missing argument -i <INPUT FILE>");
-  }
+    if(exists(inputfile)) {
+        translate_file(inputfile);
+    }else{
+        writeln("Missing argument -i <INPUT FILE>");
+    }
 }
